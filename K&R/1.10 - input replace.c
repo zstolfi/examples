abd84410d613e963a8beefeb3ccc9c89e6279934
TheZacher5645 <zander5645@yahoo.com>
2023-02-08 05:13:30 -0600

    Finish 1.12

--- K&R/1.10 - input replace.c
+++ K&R/1.10 - input replace.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 main() {
-	int c;
-	while ((c = getchar()) != EOF) {
+	for (int c; (c = getchar()) != EOF; ) {
 		/**/ if (c == '\t') { putchar('\\'); putchar('t'); }
 		else if (c == '\b') { putchar('\\'); putchar('b'); }
 		else if (c == '\\') { putchar('\\'); putchar('\\'); }
--- K&R/1.12 - word splitter.c
+++ K&R/1.12 - word splitter.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+#define WORDS       0
+#define FIRST_SPACE 1
+#define SPACES      2
+
+#define isWhitespace(c) (c == ' ' || c == '\t' || c == '\n')
+
 /* print input one word per line */
 main () {
-	int c;
+	int c, state;
+
+	state = WORDS;
 	while ((c = getchar()) != EOF) {
-		/**/ if (c == ' ' ) { putchar('\n'); }
-		else if (c == '\n') { putchar('\n'); }
-		else /*          */ { putchar(c);    }
+		/**/ if (state == WORDS      ) { state = isWhitespace(c) ? FIRST_SPACE : WORDS; }
+		else if (state == FIRST_SPACE) { state = isWhitespace(c) ? SPACES      : WORDS; }
+		else if (state == SPACES     ) { state = isWhitespace(c) ? SPACES      : WORDS; }
+
+		if (state == WORDS)       { putchar(c); }
+		if (state == FIRST_SPACE) { putchar('\n'); }
+		if (state == SPACES)      { /*do nothing*/ }
 	}
 }
