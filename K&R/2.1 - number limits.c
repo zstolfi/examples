87505a611daca6004dd39c6b460137338e785f1a
TheZacher5645 <zander5645@yahoo.com>
2023-02-10 10:32:35 -0600

    Continue Exercise 2.1
    (Now it works for unsigned int & unsigned long)

--- K&R/2.1 - number limits.c
+++ K&R/2.1 - number limits.c
@@ -14,16 +14,64 @@ int main() {
 	// O(n) time
 	unsigned char ucharMin = 0, ucharMax = 0, next_uchar;
 	signed char charMin = 0, charMax = 0, next_char;
-	next_uchar = ucharMax; while (++next_uchar >  0) { ucharMax++; }
-	next_char  = charMin;  while (--next_char  <  0) { charMin--;  }
-	next_char  = charMax;  while (++next_char  >= 0) { charMax++;  }
+	next_uchar = 0; while (++next_uchar >  0) { ucharMax++; }
+	next_char  = 0; while (--next_char  <  0) { charMin--;  }
+	next_char  = 0; while (++next_char  >= 0) { charMax++;  }
 
 	unsigned short ushortMin = 0, ushortMax = 0, next_ushort;
 	signed short shortMin = 0, shortMax = 0, next_short;
-	next_ushort = ushortMax; while (++next_ushort >  0) { ushortMax++; }
-	next_short  = shortMin;  while (--next_short  <  0) { shortMin--;  }
-	next_short  = shortMax;  while (++next_short  >= 0) { shortMax++;  }
+	next_ushort = 0; while (++next_ushort >  0) { ushortMax++; }
+	next_short  = 0; while (--next_short  <  0) { shortMin--;  }
+	next_short  = 0; while (++next_short  >= 0) { shortMax++;  }
 
+	// Better method, increase then decrease the step size
+	// O(log(n)) time
+	long step = 1;
+	long s2;
+
+	#define increaseStep() \
+	    s2 = step*2; \
+	    step = (s2 > step) ? s2 : LONG_MAX;
+
+	#define decreaseStep() \
+	    s2 = step/2; \
+	    step = (s2 < step) ? s2 : 0;
+
+
+	unsigned int uintMin = 0, uintMax = 0, next_uint;
+	signed int intMin = 0, intMax = 0, next_int;
+	next_uint = 0;
+	step = 1;
+	do { // widening steps
+		uintMax = next_uint;
+		next_uint += step;
+		increaseStep();
+	} while (next_uint > uintMax);
+	do { // diminishing steps
+		next_uint = uintMax;
+		while ((next_uint += step) > uintMax) {
+			uintMax = next_uint;
+		}
+		decreaseStep();
+	} while (step > 0);
+
+
+	unsigned long ulongMin = 0, ulongMax = 0, next_ulong;
+	signed long longMin = 0, longMax = 0, next_long;
+	next_ulong = 0;
+	step = 1;
+	do { // widening steps
+		ulongMax = next_ulong;
+		next_ulong += step;
+		increaseStep();
+	} while (next_ulong > ulongMax);
+	do { // diminishing steps
+		next_ulong = ulongMax;
+		while ((next_ulong += step) > ulongMax) {
+			ulongMax = next_ulong;
+		}
+		decreaseStep();
+	} while (step > 0);
 
 
 	printf("x~~~~~~~~~~~~x\n");
@@ -44,23 +92,23 @@ int main() {
 	printf("signed computed  :\t%hd - %hd\n", shortMin, shortMax);
 	printf("\n");
 
-	// printf("x~~~~~~~~~~~x\n");
-	// printf("|    INT    |\n");
-	// printf("x~~~~~~~~~~~x\n");
-	// printf("unsigned         :\t%u - %u\n", (unsigned int)0, UINT_MAX);
-	// printf("unsigned computed:\t%u - %u\n", uintMin, uintMax);
-	// printf("signed           :\t%d - %d\n", INT_MIN, INT_MAX);
-	// printf("signed computed  :\t%d - %d\n", intMin, intMax);
-	// printf("\n");
+	printf("x~~~~~~~~~~~x\n");
+	printf("|    INT    |\n");
+	printf("x~~~~~~~~~~~x\n");
+	printf("unsigned         :\t%u - %u\n", (unsigned int)0, UINT_MAX);
+	printf("unsigned computed:\t%u - %u\n", uintMin, uintMax);
+	printf("signed           :\t%d - %d\n", INT_MIN, INT_MAX);
+	printf("signed computed  :\t%d - %d\n", intMin, intMax);
+	printf("\n");
 
-	// printf("x~~~~~~~~~~~~x\n");
-	// printf("|    LONG    |\n");
-	// printf("x~~~~~~~~~~~~x\n");
-	// printf("unsigned         :\t%lu - %lu\n", (unsigned long)0, ULONG_MAX);
-	// printf("unsigned computed:\t%lu - %lu\n", ulongMin, ulongMax);
-	// printf("signed           :\t%ld - %ld\n", LONG_MIN, LONG_MAX);
-	// printf("signed computed  :\t%ld - %ld\n", longMin, longMax);
-	// printf("\n");
+	printf("x~~~~~~~~~~~~x\n");
+	printf("|    LONG    |\n");
+	printf("x~~~~~~~~~~~~x\n");
+	printf("unsigned         :\t%lu - %lu\n", (unsigned long)0, ULONG_MAX);
+	printf("unsigned computed:\t%lu - %lu\n", ulongMin, ulongMax);
+	printf("signed           :\t%ld - %ld\n", LONG_MIN, LONG_MAX);
+	printf("signed computed  :\t%ld - %ld\n", longMin, longMax);
+	printf("\n");
 
 	return 0;
 }
