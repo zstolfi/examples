4a94f1b47e12a685eabb83dd732c24397a579a83
TheZacher5645 <zander5645@yahoo.com>
2023-03-06 18:16:19 -0600

    Detect/Fill zero windows

--- number walls/main.cc
+++ number walls/main.cc
@@ -7,7 +7,9 @@
 
 template <typename T>
 class NumberWall {
+private:
 	using brick = std::optional<T>; // haha, bricks, get it? :^)
+	struct window { int size, m, n; };
 	std::function<T(int)> sequence {};
 	std::vector<brick> wall {};
 
@@ -15,14 +17,15 @@ class NumberWall {
 	const int x0, x1;   	// x bounds
 	const int y0, y1;   	// y bounds
 
+	std::vector<window> zeroWindows {};
+	// std::vector<window> partialZeroWindows {};
+
 public:
 	NumberWall(std::function<T(int)> s, int x, unsigned w, unsigned h)
 	: sequence{s}, w{w}, h{h}
 	, x0{x}, x1 {x+w}
 	, y0{0}, y1 {0+h}  {
 		wall.resize(w*h, std::nullopt);
-		
-		
 	}
 
 	const std::vector<T>& data() { return wall; }
@@ -31,28 +34,24 @@ public:
 
 
 
+public:
 	/* Calculation */
 	void calculate() {
-		auto get = [&](int m, int n) -> brick {
-			const unsigned i = m, j = n-x0;
-			// if (m >= 0) { std::cout << m << "," << n << "\t" << i << ", " << j << "\n"; }
-			/**/ if (m <= -2) { return 0; }
-			else if (m == -1) { return 1; }
-			else if (0 <= j&&j < w && i < h) { return wall[i*w+j]; }
-			else { return std::nullopt; }
-		};
-
-		auto set = [&](int m, int n, brick value) {
-			const unsigned i = m, j = n-x0;
-			// assert(0 <= i&&i < h);
-			// assert(0 <= j&&j < w);
-			wall[i*w+j] = value;
-		};
-
-		for (int n=x0; n < x1; n++) {
+		// setup
+		for (int n=x0; n < x1; n++)
 			wall[n-x0] = sequence(n);
-		}
 
+		//run the cross rule
+		crossRule();
+		findZeroWindows();
+		fillZeroWindows();
+	}
+
+
+
+private:
+	/* Calculation Methods */
+	void crossRule() {
 		for (int m=y0; m < y1; m++) {
 		for (int n=x0; n < x1; n++) {
 			// https://files.catbox.moe/vlh72e.png
@@ -63,15 +62,66 @@ public:
 			brick c  = get(m, n);
 
 			if (!a0 + !a1 + !b0 + !b1 + !c == 1) { // 1 unknown
-				/**/ if (!a0 && a1 != 0) { set(m-1, n, ((*c)*(*c) - (*b0)*(*b1)) / (*a1)); }
-				else if (!a1 && a0 != 0) { set(	m+1, n, ((*c)*(*c) - (*b0)*(*b1)) / (*a0)); }
-				else if (!b0 && b1 != 0) { set(m, n-1, ((*c)*(*c) - (*a0)*(*a1)) / (*b1)); }
-				else if (!b1 && b0 != 0) { set(m, n+1, ((*c)*(*c) - (*a0)*(*a1)) / (*b0)); }
+				/**/ if (!a0 && a1 != 0) { set(m-1, n, T{((*c)*(*c) - (*b0)*(*b1)) / (*a1)}); }
+				else if (!a1 && a0 != 0) { set(m+1, n, T{((*c)*(*c) - (*b0)*(*b1)) / (*a0)}); }
+				else if (!b0 && b1 != 0) { set(m, n-1, T{((*c)*(*c) - (*a0)*(*a1)) / (*b1)}); }
+				else if (!b1 && b0 != 0) { set(m, n+1, T{((*c)*(*c) - (*a0)*(*a1)) / (*b0)}); }
 			}
 		} }
 	}
 
+	void findZeroWindows() {
+		for (int m=y0; m < y1; m++) {
+		for (int n=x0; n < x1; n++) {
+			brick c  = get(m,n);
+			brick a  = get(m-1, n);
+			brick b0 = get(m, n-1);
+			brick b1 = get(m, n+1);
+			if (( c  && *c  == 0)
+			&&  ( a  && *a  != 0)
+			&&  (!b0 || *b0 != 0)) {
+				window win {1, m, n};
+				while (b1 && b1 == 0) {
+					win.size++;
+					n++;
+					b1 = get(m, n+1);
+				}
+				zeroWindows.push_back(win);
+			}
+		} }
+	}
 
+	void fillZeroWindows() {
+		for (auto& win : zeroWindows) {
+			for (int m =win.m; m < win.m + win.size; m++) {
+			for (int n =win.n; n < win.n + win.size; n++) {
+				set(m, n, T{0});
+			} }
+		}
+	}
+
+
+
+private:
+	/* Internal Functions */
+	brick get(int m, int n) {
+		const unsigned i = m, j = n-x0;
+		/**/ if (m <= -2) { return T{0}; }
+		else if (m == -1) { return T{1}; }
+		else if (0 <= j&&j < w && i < h) { return wall[i*w+j]; }
+		else { return std::nullopt; }
+	};
+
+	void set(int m, int n, brick value) {
+		const unsigned i = m, j = n-x0;
+		if ((0 <= i&&i < h) && (0 <= j&&j < w)) {
+			wall[i*w+j] = value;
+		}
+	};
+
+
+
+public:
 	/* Print Functions */
 	void print() {
 		for (unsigned i=0; i < h; i++) {
@@ -90,7 +140,7 @@ public:
 	void printPattern(std::function<bool(T)> f) {
 		for (unsigned i=0; i < h; i++) {
 			for (unsigned j=0; j < w; j++) {
-				if (auto value = wall[i*w+j]; value || f(*value)) {
+				if (auto value = wall[i*w+j]; value && f(*value)) {
 					std::cout << "##";
 				} else {
 					std::cout << "  ";
@@ -104,20 +154,27 @@ public:
 
 
 #include <gmpxx.h>
-#include "fib.hh"
+#include "sequences.hh"
 using INT = mpz_class;
 
 
 int main() {
 	NumberWall<INT> squares {
 		// [](auto n) -> INT { return n*n; } ,
-		[](auto n) -> INT { return Fib(n); } ,
-		5 ,
-		10, 5
+		// [](auto n) -> INT { return n*n*n; } ,
+		// Fib ,
+		// seq_ZeroWindows ,
+		[](auto n) {
+			const std::vector<INT> seq = {1,0,0,0,0,0,0,0,1};
+			return (0 <= n&&n < seq.size()) ? seq[n] : 0;
+		} ,
+		0 ,
+		14, 10
 	};
 
 	squares.calculate();
 	squares.print();
+	squares.printPattern([](auto n) { return true; });
 
 	return 0;
 }
--- number walls/sequences.hh
+++ number walls/sequences.hh
@@ -1,6 +1,6 @@
 #include <gmpxx.h>
 
-mpz_class Fib(int n) { // valid for n = [-100 ... 100]
+mpz_class seq_Fib(int n) { // valid for n = [-100 ... 100]
 	const mpf_class phi      {"1.61803398874989484820458", 80};
 	const mpf_class invsqrt5 {"0.44721359549995793928183", 80};
 
@@ -10,4 +10,10 @@ mpz_class Fib(int n) { // valid for n = [-100 ... 100]
 	result *= (n >= 0 || n%2 == 0) ? 1 : -1;
 	result = floor(0.5 + result);
 	return mpz_class{result};
+}
+
+// https://www.youtube.com/watch?v=NO1_-qptr6c&t=904
+mpz_class seq_ZeroWindows(int n) {
+	bool a = mpz_perfect_square_p(mpz_class{8*n + 9}.get_mpz_t());
+    return mpz_class{a ? 1 : 0};
 }
