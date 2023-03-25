e39870a3fdf1499eb1fee0103c6866381049e5bf
TheZacher5645 <zander5645@yahoo.com>
2023-03-24 20:44:19 -0500

    update main calculator program

--- K&R/reverse polish calc.c
+++ K&R/reverse polish calc.c
@@ -86,14 +86,14 @@ void ungetch(int);
 int getop(char s[]) {
 	int i, c;
 
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
+	while (s[0] = c = getch(), c == ' ' || c == '\t')
 		;
 	s[1] = '\0';
 	if (!isdigit(c) && c != '.')
 		return c;    /* not a number */
 	i = 0;
 	if (isdigit(c))    /* collect integer part */
-		while (isdigit(s[++i] = c = getch()))
+		while (s[++i] = c = getch(), isdigit(c))
 			;
 	s[i] = '\0';
 	if (c != EOF)
