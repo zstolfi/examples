00c52553e1ef9c8fdede967ec0645ed5ec349fd2
TheZacher5645 <zander5645@yahoo.com>
2023-04-11 09:24:36 -0500

    Draw using font data

--- font display/draw.hh
+++ font display/draw.hh
@@ -5,13 +5,21 @@
 
 class Canvas {
 private:
-	unsigned* pixels;
+	using Color = unsigned;
+
+	Color* pixels;
 	const int W;
 	const int H;
 
+	void put(int x, int y, Color c) {
+		if ((0 <= x&&x < W) && (0 <= y&&y < H)) {
+			pixels[y * W + x] = c;
+		}
+	}
+
 public:
 	Canvas(SDL_Surface* surface)
-	: pixels{(unsigned*)surface->pixels}
+	: pixels{(Color*)surface->pixels}
 	, W{surface->w}
 	, H{surface->h} {}
 
@@ -20,6 +28,22 @@ public:
 			pixels[i] = 0xFFFFFFFF;
 		}
 
+		const Font& font = fonts.at("font 2");
 
+		int cursorX = 10;
+		int cursorY = 10;
+		for (char c : s.text) {
+			if (!font.contains(c)) { continue; }
+			const CharData& glyph = font.at(c);
+			
+			for (unsigned y=0; y < glyph.h; y++) {
+			for (unsigned x=0; x < glyph.w; x++) {
+				if (glyph.pixels[y * glyph.w + x]) {
+					put(cursorX+x, cursorY+y, 0xED1C24);
+				}
+			} }
+
+			cursorX += glyph.w + 3;
+		}
 	}
 };
