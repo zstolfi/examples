0af39b803a3e3fb3abead3204fd4c195bfbd4d3c
TheZacher5645 <zander5645@yahoo.com>
2023-04-11 08:18:42 -0500

    Input font data
    Ideally this would be its own text file instead of a source file, but hey if it works

--- font display/draw.hh
+++ font display/draw.hh
@@ -1,6 +1,7 @@
 #pragma once
 #include <SDL.h>
 #include "game state.hh"
+#include "fonts.hh"
 
 class Canvas {
 private:
@@ -16,10 +17,9 @@ public:
 
 	void draw(GameState& s) {
 		for (int i=0; i < W*H; i++) {
-			unsigned x = i%W;
-			unsigned y = i/W;
-
-			pixels[i] = 0xFF<<24 | (i + s.frameCount*s.frameCount)*(x/8%2 ^ y/8%2);
+			pixels[i] = 0xFFFFFFFF;
 		}
+
+
 	}
 };
