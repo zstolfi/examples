6f2e79c3db05e7b856242a471b8859e632957fec
TheZacher5645 <zander5645@yahoo.com>
2023-04-25 21:26:20 -0500

    v1 of fixed point finder
    I'll experiment with adding 'offsets' soon, maybe fixed points are easier to find that way? I can hope

--- javascript/find fixed point.js
+++ javascript/find fixed point.js
@@ -12,22 +12,47 @@ let tries = [];
 let loops = [];
 
 function compare(c, d) {
-	return undefined == [0,1,2,3,4,5,6,7,8,9].find( (i) => c[i] != d[i] );
+	let index = [0,1,2,3,4,5,6,7,8,9].find( (i) => c[i] != d[i] );
+	if (index == undefined) { return 0; }
+	return c[index] - d[index];
 }
 
 function indexOf(c) {
-	return tries.findIndex( (d) => compare(c,d) );
+	return tries.findIndex( (d) => compare(c,d) == 0 );
 }
 
 
+// It seems there are only 2 possible loops in base 10
+// https://www.youtube.com/watch?v=1GKfEDvhWdY
+t0 = [0,0,0,0,0,0,0,0,0,0];
+while (loops.length < 2) {
+	t0[0]++;
+	for (let i=0; i<10; i++) {
+		if (t0[i] >= 10) {
+			let digit = t0[i] % 10;
+			let carry = (t0[i] - digit) / 10;
+			t0[i] = digit;
+			t0[i+1] += carry;
+		}
+	}
+	t = t0;
 
-t = [0,0,0,0,0,0,0,0,0,0];
-
-while (indexOf(t) == -1) {
-	tries.push(t);
-	t = count(t);
-} /**/ {
-	loops.push({c: t, length: tries.length - indexOf(t)});
+	let loopIndex;
+	while ((loopIndex = indexOf(t)) == -1) {
+		tries.push(t);
+		t = count(t);
+	} /**/ {
+		let loopLength = tries.length - loopIndex;
+		// store the unique 'minimum' value of the loop, just to prevent duplicate entires
+		let min = [Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,Infinity];
+		for (let i=loopIndex; i<tries.length; i++) {
+		    if (compare(tries[i], min) < 0) { min = tries[i]; }
+		}
+		if (!loops.find( (loop) => compare(loop.c, min) == 0 )) {
+			loops.push({c: min, length: loopLength});
+			if (loopLength == 1) { console.log("Fixed point found!", min); }
+		}
+		tries = [];
+	}
 }
-
 console.log(loops);
