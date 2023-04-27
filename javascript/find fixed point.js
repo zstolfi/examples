2de7894ec8fc7d00c82b46271248ce631c3c425a
TheZacher5645 <zander5645@yahoo.com>
2023-04-27 15:34:55 -0500

    Update fixed point searcher

--- javascript/find fixed point.js
+++ javascript/find fixed point.js
@@ -1,14 +1,21 @@
+const MAX_TRIES = 1000000;
+const offset = [1,1,1,1,1,1,1,1,1,1];
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 function count(list) {
 	let result = [0,0,0,0,0,0,0,0,0,0];
 	for (let number of list) {
 		for (let digit of number.toString()) {
 			result[parseInt(digit)]++;
 	} }
+	// optional offset
+	for (let i=0; i<10; i++) { result[i] += offset[i]; }
 	return result;
 }
 
-let t;
-let tries = [];
+let t = [0,0,0,0,0,0,0,0,0,0];
+let list = [];
 let loops = [];
 
 function compare(c, d) {
@@ -18,15 +25,13 @@ function compare(c, d) {
 }
 
 function indexOf(c) {
-	return tries.findIndex( (d) => compare(c,d) == 0 );
+	return list.findIndex( (d) => compare(c,d) == 0 );
 }
 
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-// It seems there are only 2 possible loops in base 10
-// https://www.youtube.com/watch?v=1GKfEDvhWdY
-t0 = [0,0,0,0,0,0,0,0,0,0];
-while (loops.length < 2) {
-	t0[0]++;
+let tries = 0;
+for (let t0=[0,0,0,0,0,0,0,0,0,0]; ; t0[0]++) {
 	for (let i=0; i<10; i++) {
 		if (t0[i] >= 10) {
 			let digit = t0[i] % 10;
@@ -35,24 +40,27 @@ while (loops.length < 2) {
 			t0[i+1] += carry;
 		}
 	}
-	t = t0;
+	for (let i=0; i<10; i++) { t[i] = t0[i]; }
 
 	let loopIndex;
 	while ((loopIndex = indexOf(t)) == -1) {
-		tries.push(t);
+		list.push(t);
 		t = count(t);
 	} /**/ {
-		let loopLength = tries.length - loopIndex;
+		let loopLength = list.length - loopIndex;
 		// store the unique 'minimum' value of the loop, just to prevent duplicate entires
 		let min = [Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,Infinity];
-		for (let i=loopIndex; i<tries.length; i++) {
-		    if (compare(tries[i], min) < 0) { min = tries[i]; }
+		for (let i=loopIndex; i<list.length; i++) {
+		    if (compare(list[i], min) < 0) { min = list[i]; }
 		}
 		if (!loops.find( (loop) => compare(loop.c, min) == 0 )) {
 			loops.push({c: min, length: loopLength});
 			if (loopLength == 1) { console.log("Fixed point found!", min); }
 		}
-		tries = [];
+		// console.log(list);
+		list = [];
+		if (tries++ > MAX_TRIES) { break; }
 	}
 }
+console.log(`Search complete. ${loops.length} loops found.`);
 console.log(loops);
