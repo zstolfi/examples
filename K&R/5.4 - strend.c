7158ffdc8818f9136f3c71e33d43475de7ff6220
TheZacher5645 <zander5645@yahoo.com>
2023-05-23 03:53:17 -0500

    Finish exercise 5.4

---
+++ K&R/5.4 - strend.c
@@ -0,0 +1,45 @@
+/* Exercise 5-4.
+	Write the function strend(s,t), which returns 1 if the
+	string t occurs at the end of the string s, and zero
+	otherwise.
+*/
+
+#include <stdbool.h>
+
+bool strend(char* s, char* t) {
+	char* s0 = s;
+	char* t0 = t;
+	while (*s != '\0') s++;
+	while (*t != '\0') t++;
+	if (t-t0 > s-s0)  /* t is longer than s, meaning */
+		return false; /* s cannot possibly contain t */
+
+	while (t >= t0) /* iterate and check backwards */
+		if (*s-- != *t--) return false;
+
+	return true;
+}
+
+
+
+#include <stdio.h>
+
+void test(char* str1, char* str2) {
+	printf("(%d) '%s' '%s'\n",
+	       strend(str1,str2),
+	       str1,
+	       str2);
+}
+
+int main(void) {
+	test("I know a thing or two about her.",
+	     "about");
+	test("e = 2.718281828",
+	     "1828");
+	test("trivial",
+	     "");
+	test("string",
+	     "longer string");
+
+	return 0;
+}
