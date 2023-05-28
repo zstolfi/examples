b8585a113f9ddb57cb7199ae23566d1659b139dd
TheZacher5645 <zander5645@yahoo.com>
2023-05-27 20:28:18 -0500

    Finish first-last problem
    Logically this was a first-try result
    But language-wise I was snagged on like 3 different things

--- leetcode/find-first-and-last-position-of-element-in-sorted-array.cc
+++ leetcode/find-first-and-last-position-of-element-in-sorted-array.cc
@@ -1,44 +1,53 @@
 class Solution {
 private:
-	using Array = std::vector<int>;
-	using Iterator = Array::iterator;
+    using Array = std::vector<int>;
+    using Iterator = Array::iterator;
 
-	Iterator middle(Iterator begin,
-	                Iterator end) {
-		return std::next(begin, std::distance(begin, end) / 2);
-	}
+    static Iterator middle(Iterator begin,
+                    Iterator end) {
+        return std::next(begin, (end - begin) / 2);
+    }
 
-	// Finds the boundary between false and true, (last if none found)
-	// [F,F,F,F,F,F,F,F,F,T,T,T,T]  
-	//                   (^)        
-	// [T,T,T,T,T,T,T,T,T,T,T,T,T]  
-	// (^)                          
-	// [F,F,F,F,F,F,F,F,F,F,F,F,F]  
-	//                           (^)
-	Iterator binarySearch(Iterator begin,
-	                      Iterator end,
-	                      auto&& predicate,
-	                      Iterator hint=middle(begin,end)) {
-		for (Iterator it = hint; end-begin > 1; ) {
-			if (predicate(*it)) { end   = std::prev(it); }
-			else                { begin = std::next(it); }
-			it = middle(begin, end);
-		}
-		if (begin == end) { return end; }
-		return predicate(*begin)
-			? begin // [T]   [F]
-			: end;  // (^)     (^)
-	}
+    // Finds the boundary between false and true, (last if none found)
+    // [F,F,F,F,F,F,F,F,F,T,T,T,T]  
+    //                   (^)        
+    // [T,T,T,T,T,T,T,T,T,T,T,T,T]  
+    // (^)                          
+    // [F,F,F,F,F,F,F,F,F,F,F,F,F]  
+    //                           (^)
+    template <typename PredFunc>
+    Iterator binarySearch(Iterator begin,
+                          Iterator end,
+                          PredFunc predicate,
+                          Iterator hint) {
+        for (Iterator it = hint; end-begin > 1; ) {
+            if (predicate(*it)) { end   = /*     */(it); }
+            else                { begin = std::next(it); }
+            it = middle(begin, end);
+        }
+        if (begin == end) { return end; }
+        return predicate(*begin)
+            ? begin // [T]   [F]
+            : end;  // (^)     (^)
+    }
+
+    template <typename PredFunc> // function overload, for a default hint value
+    Iterator binarySearch(Iterator b, Iterator e, PredFunc p)
+        { return binarySearch(b, e, p, middle(b,e)); }
+
 public:
-	vector<int> searchRange(Array& nums, int target) {
-		auto untilTargetBegin = [target](int n) { return n >= i; };
-		auto untilTargetEnd   = [target](int n) { return n >  i; };
+    vector<int> searchRange(Array& nums, int target) {
+        auto untilTargetBegin = [target](int n) { return n >= target; };
+        auto untilTargetEnd   = [target](int n) { return n >  target; };
 
-		Iterator it0 = binarySearch(nums.begin(), nums.end(), untilTargetBegin);
-		Iterator it1 = binarySearch(it0         , nums.end(), untilTargetEnd, it0);
+        Iterator it0 = binarySearch(nums.begin(), nums.end(), untilTargetBegin);
+        Iterator it1 = binarySearch(it0         , nums.end(), untilTargetEnd, it0);
 
-		return (it0 < it1)
-			? {it0 - nums.begin(), --it1 - nums.begin()}
-			: {-1, -1};
-	}
+        std::vector<int> result = {-1, -1};
+        if (it0 < it1) {
+            result[0] = /*     */(it0) - nums.begin();
+            result[1] = std::prev(it1) - nums.begin();
+        }
+        return result;
+    }
 };
