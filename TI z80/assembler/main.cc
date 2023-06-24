ae50531698113da140d3cc7aa28fa1ca0bffb13f
TheZacher5645 <zander5645@yahoo.com>
2023-06-24 09:38:25 -0500

    Add error checking for cmd args

--- TI z80/assembler/main.cc
+++ TI z80/assembler/main.cc
@@ -9,6 +9,8 @@ void printStatus (const char*);
 void printWarning(const char*);
 void printError  (const char*);
 
+
+
 struct Arguments {
 	fls::path sourcePath;
 	fls::path outputPath;
@@ -17,8 +19,13 @@ struct Arguments {
 
 	Arguments(int argc, char* argv[]) {
 		for (int i=1; i<argc; i++) {
-			if (argv[i] == "-i"s) { i++; sourcePath = argv[i]; continue; }
-			if (argv[i] == "-o"s) { i++; outputPath = argv[i]; continue; }
+			if (argv[i][0] != '-') { printError("unexpected cmd arg"); }
+			char flag = argv[i++][1];
+			if (i >= argc || argv[i][0] == '-') { printError("missing cmd arg"); }
+
+			/**/ if (flag == 'i') { sourcePath = argv[i]; }
+			else if (flag == 'o') { outputPath = argv[i]; }
+			else    { printError("unknown cmd flag"); }
 		}
 		if (!sourcePath.empty()) { sourceStream = std::ifstream(sourcePath); }
 		if (!outputPath.empty()) { outputStream = std::ofstream(outputPath); }
@@ -36,3 +43,9 @@ int main(int argc, char* argv[]) {
 	for (std::string line; std::getline(source, line); )
 		output << line << "\n";
 }
+
+
+
+void printStatus (const char* msg) { std::cout                 << msg; }
+void printWarning(const char* msg) { std::cout << "Warning:\t" << msg; }
+void printError  (const char* msg) { std::cout << "Error:\t"   << msg; exit(1); }
