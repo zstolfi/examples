167b7ff1e2a0b227d46e9f85edf175d4218a620b
TheZacher5645 <zander5645@yahoo.com>
2023-10-02 14:50:45 -0500

    Add files via upload

--- leetcode/trapping-rain-water.cc
+++ leetcode/trapping-rain-water.cc
@@ -4,39 +4,46 @@ class Solution {
 
     struct End {}; // start or end of graph (no water allowed)
     using Pos = std::variant<std::size_t, End>;
+    enum struct InflectionKind { Peak, Valley };
 
     struct Inflection {
         Pos leftEdge;  // Count 'plateaus' as one cusp
         Pos rightEdge; // with two edges on each side.
     };
 
-    auto calcInflectionPoints(const std::vector<int>& height) {
-        std::vector<Inflection> result = {};
-        // if (height.size() == 1) return result = {{End{}, End{}}};
-
+    std::vector<Inflection> inflections;
+    InflectionKind firstKind;
+    
+    void calcInflectionPoints(const std::vector<int>& height) {
         auto sign = [](auto x) { return (x<0) ? -1 : (x>0) ? 1 : 0; };
 
-        result.emplace_back(End{}, 0uz);
+        inflections.emplace_back(End{}, 0uz);
         int dirPrev = sign(height[1] - height[0]);
         std::size_t posPrev = 1;
         for (std::size_t i=2; i<height.size(); i++) {
             int dir = sign(height[i] - height[i-1]);
-            if (dir * dirPrev == -1) { // change in direction
-                result.emplace_back(posPrev, i-1);
+
+            if (inflections.size() == 1 && dir != 0) {
+                firstKind = (dir == +1) ? InflectionKind::Peak
+                            /*dir == -1*/: InflectionKind::Valley;
             }
 
-            // starting ledge
-            if (dirPrev == 0) result.front().rightEdge = i-1;
-            // flat spot
+            if (dir * dirPrev == -1) { // change in direction
+                inflections.emplace_back(posPrev, i-1);
+            }
+
+            // flat spot at start
+            if (dirPrev == 0) inflections.front().rightEdge = i-1;
+            // flat spot elsewhere
             if (dir != 0) dirPrev = dir, posPrev = i;
         }
-        result.emplace_back(posPrev, End{});
-
-        return result;
+        inflections.emplace_back(posPrev, End{});
+        if (dirPrev == 0) { inflections = {{End{}, End{}}}; return; }
     }
 
 public:
     int trap(std::vector<int>& height) {
-        auto inflections = calcInflectionPoints(height);
+        calcInflectionPoints(height);
+        if (inflections.size() <= 2) return 0;
     }
 };
