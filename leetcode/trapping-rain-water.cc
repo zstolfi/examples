392af016cf6ff1f0531ed4fcfad7c7fcfb4b2466
TheZacher5645 <zander5645@yahoo.com>
2023-10-02 21:12:16 -0500

    Finish writing trapping-water (untested)

--- leetcode/trapping-rain-water.cc
+++ leetcode/trapping-rain-water.cc
@@ -6,12 +6,14 @@ class Solution {
 private:
     auto findAllPeaks(const std::vector<int>& graph) {
         std::vector<std::size_t> result {};
-        std::size_t highestPeak = 0; // leftmost peak if more than one
+        std::size_t highestPeakL = 0; // There may be multiple peaks
+        std::size_t highestPeakR = 0; // having the same max height.
 
         auto sign = [] (auto x) { return (x>0) ? 1 : (x<0) ? -1 : 0; };
         auto push = [&](auto i) {
             result.push_back(i);
-            if (graph[i] > graph[highestPeak]) highestPeak = i;
+            if (graph[i] >  graph[highestPeakL]) highestPeakL = i;
+            if (graph[i] >= graph[highestPeakR]) highestPeakR = i;
         };
 
         enum { Begin, Ppos, Pneg } state = Begin;
@@ -26,37 +28,46 @@ private:
         }
         if (state == Ppos) push(i);
 
-        return std::pair { result, highestPeak };
+        return std::tuple { result, highestPeakL, highestPeakR };
     }
 
 public:
     int trap(std::vector<int>& graph) {
-        auto [peaks, highestPeak] = findAllPeaks(graph);
+        auto [peaks, maxL, maxR] = findAllPeaks(graph);
         // slopes / hills cannot contain water
         if (peaks.size() < 2) return 0;
 
+        auto area = [&graph](std::size_t i0, std::size_t i1, int height) {
+            auto it0 = std::next(graph.begin(),i0);
+            auto it1 = std::next(graph.begin(),i1);
+            return (i1 - i0) * height - std::accumulate(it0, it1, 0,
+                [](int a, int b) {
+                    return a + std::min(height, b);
+                }
+            );
+        };
+
         int waterMass = 0;
         { // left side of the max-peak
-            std::size_t i = 0;
-            int height = graph[peaks[i]];
-            while (peaks[i] != highestPeak) {
-                for (std::size_t j=i+1; graph[peaks[j]] <= height; j++)
+            auto it = peaks.begin();
+            int height = graph[*it];
+            while (*it != maxL) {
+                for (auto j=std::next(it); graph[*j] <= height; ++j)
                     ;
-                waterMass += area(peaks[i], peaks[j], height);
-                i=j, height = graph[peaks[j]];
+                waterMass += area(*it, *j, height);
+                it=j, height = graph[*j];
             }
         } { // right side
-            std::size_t i = peaks.size()-1;
-            int height = graph[peaks[i]];
-            // In case there are multiple peaks with the
-            // max height, we stop at the rightmost one.
-            while (graph[peaks[i]] != graph[highestPeak]) {
-                for (std::size_t j=i-1; graph[peaks[j]] <= height; j--)
+            auto it = peaks.rbegin();
+            int height = graph[*it];
+            while (*it != maxR) {
+                for (auto j=std::prev(it); graph[*j] <= height; ++j)
                     ;
-                waterMass += area(peaks[j], peaks[i], height);
-                i=j, height = graph[peaks[j]];
+                waterMass += area(*j, *it, height);
+                it=j, height = graph[*j];
             }
         }
-        /* sum between the 1+ highest peaks */
+        waterMass += area(maxL, maxR, graph[maxL]);
+        return waterMass;
     }
 };
