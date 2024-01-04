53d84a660327a861a1818a687a68483ec8c79844
TheZacher5645 <zander5645@yahoo.com>
2024-01-04 07:31:50 -0600

    Board data type

--- leetcode/sudoku-solver.cc
+++ leetcode/sudoku-solver.cc
@@ -3,7 +3,7 @@ class Solution {
 
 	// TODO: constexpr generate the data
 	static constexpr auto groups = std::array {
-		std::array {  0,  1,  2,  3,  4,  5,  6,  7,  8 },  // ROWS //
+		std::array {  0,  1,  2,  3,  4,  5,  6,  7,  8 },  // 1x9 ROWS //
 		std::array {  9, 10, 11, 12, 13, 14, 15, 16, 17 },
 		std::array { 18, 19, 20, 21, 22, 23, 24, 25, 26 },
 		std::array { 27, 28, 29, 30, 31, 32, 33, 34, 35 },
@@ -13,7 +13,7 @@ class Solution {
 		std::array { 63, 64, 65, 66, 67, 68, 69, 70, 71 },
 		std::array { 72, 73, 74, 75, 76, 77, 78, 79, 80 },
 
-		std::array {  0,  9, 18, 27, 36, 45, 54, 63, 72 },  // COLUMNS //
+		std::array {  0,  9, 18, 27, 36, 45, 54, 63, 72 },  // 9x1 COLUMNS //
 		std::array {  1, 10, 19, 28, 37, 46, 55, 64, 73 },
 		std::array {  2, 11, 20, 29, 38, 47, 56, 65, 74 },
 		std::array {  3, 12, 21, 30, 39, 48, 57, 66, 75 },
@@ -23,7 +23,7 @@ class Solution {
 		std::array {  7, 16, 25, 34, 43, 52, 61, 70, 79 },
 		std::array {  8, 17, 26, 35, 44, 53, 62, 71, 80 },
 
-		std::array {  0,  1,  2,  9, 10, 11, 18, 19, 20 },  // CELLS //
+		std::array {  0,  1,  2,  9, 10, 11, 18, 19, 20 },  // 3x3 CELLS //
 		std::array {  4,  5,  6, 12, 13, 14, 21, 22, 23 },
 		std::array {  7,  8,  6, 15, 16, 17, 24, 25, 26 },
 		std::array { 27, 28, 29, 36, 37, 38, 45, 46, 47 },
@@ -42,23 +42,36 @@ class Solution {
 		};
 	}
 
-	using Number = char; // 0-9
+	using Number = char; // 0-8
 	using Possibilities = std::bitset<9>;
 	using Cell = std::variant< Number, Possibilities >;
 	
 	class Board {
 		std::array<Cell,81> m_board;
 	public:
+		// Internally use int values 0-8 for solving ...
 		Board(BoardInOut& b) {
 			m_board.fill(Possibilities {0b111111111});
 			for (int i=0; i<9; i++) {
 			for (int j=0; j<9; j++) {
-				if (b[i][j] != '.') assignVal(i*9 + j, b[i][j] - '0');
+				if (b[i][j] != '.') assignVal(i*9 + j, b[i][j] - '1');
 			} }
 		}
 
+		// But convert back to 1-9 for the output.
+		operator BoardInOut() {
+			BoardInOut result = std::vector (9, std::vector (9, '.'));
+			for (unsigned i; i<81; i++) {
+				bool assigned = std::holds_alternative<Number>(m_board[i]);
+				result[i/9][i%9] = assigned ? std::get<Number>(m_board[i]) + '1' : '.';
+			}
+			return result;
+		}
+
 		void assignVal(unsigned i, char val) {
-			assert(std::holds_alternative<Possibilities>(m_board[i]) || m_board[i] == val);
+			assert(std::holds_alternative<Possibilities>(m_board[i])
+			||     std::get<Number>(m_board[i]) == val);
+			
 			m_board[i] = val;
 			for (auto groupId : groupsFromCell(i)) {
 				for (auto cellId : groups[groupId]) {
@@ -72,7 +85,13 @@ class Solution {
 	};
 
 public:
-	void solveSudoku(BoardInOut& board) {
+	void solveSudoku(BoardInOut& b) {
+		// Convert to our custom data type.
+		Board board {b};
 		
+		/* solving code */
+
+		// And convert back when done.
+		b = board;
 	}
 };
