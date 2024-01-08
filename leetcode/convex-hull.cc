7147a301eccb9ea44a89026b34debbcc85246067
TheZacher5645 <zander5645@yahoo.com>
2024-01-08 12:56:56 -0600

    Attempt 2, also a fail

--- leetcode/convex-hull.cc
+++ leetcode/convex-hull.cc
@@ -1,56 +1,60 @@
-/* convex-hull aka erect-the-fence */
 class Solution {
 	using Point = std::vector<int>;
-	static constexpr auto comps = std::array<bool(*)(Point,Point),4> {
-		[] (Point a, Point b) { return a[0] > b[0] || a[1] > b[1]; },
-		[] (Point a, Point b) { return a[1] > b[1] || a[0] < b[0]; },
-		[] (Point a, Point b) { return a[0] < b[0] || a[1] < b[1]; },
-		[] (Point a, Point b) { return a[1] < b[1] || a[0] > b[0]; },
-	};
-
+	using Iterator = std::vector<Point>::iterator;
 public:
-	std::vector<Point> outerTrees(std::vector<Point>& points) {
-		std::vector<Point> result;
-
-		const auto extrema = std::array<Point,4> {
-			*std::min_element(points.begin(), points.end(), comps[0]),
-			*std::min_element(points.begin(), points.end(), comps[1]),
-			*std::min_element(points.begin(), points.end(), comps[2]),
-			*std::min_element(points.begin(), points.end(), comps[3]),
-		};
+	std::vector<Point> outerTrees(std::vector<Point>& trees) {
+		const std::size_t N = trees.size();
+		std::sort(
+			trees.begin(), trees.end(),
+			[](Point a, Point b) { return a[0] < b[0] || a[1] < b[1]; }
+		);
+		std::vector<bool> removeIds (trees.size(), true);
 		
-		std::set<Point, decltype(comps[0])> quad0 {comps[0]};
-		std::set<Point, decltype(comps[1])> quad1 {comps[1]};
-		std::set<Point, decltype(comps[2])> quad2 {comps[2]};
-		std::set<Point, decltype(comps[3])> quad3 {comps[3]};
-		for (Point p : points) {
-			if (p[1] >= extrema[0][1] && p[0] > extrema[1][0]) quad0.insert(p);
-			if (p[0] <= extrema[1][0] && p[1] > extrema[2][1]) quad1.insert(p);
-			if (p[1] <= extrema[2][1] && p[0] < extrema[3][0]) quad2.insert(p);
-			if (p[0] >= extrema[3][0] && p[1] < extrema[0][1]) quad3.insert(p);
+		int minX = trees.front()[0];
+		int maxX = trees.back ()[0];
+
+		// 1st pass, left side of the hull.
+		int minY = 101, maxY = -1;
+		std::size_t lowestIdL = 0, highestIdL = 0;
+		for (std::size_t i=0, next=i; i<trees.size(); i=next) {
+			while (next<N && trees[next].x == trees[i].x) next++;
+			const std::size_t n = next-1;
+
+			if (trees[i].x == minX) {
+				lowestIdL = i; highestIdL = n;
+				for (std::size_t j=i; j<=n; j++) removeIds[j] = false;
+			}
+			if (trees[i].y < minY) { minY = trees[i]; lowestIdL  = i; removeIds[i] = false; }
+			if (trees[n].y > maxY) { maxY = trees[n]; highestIdL = n; removeIds[n] = false; }
+		}
+		int minYg = minY, maxYg = maxY;
+
+		// 2nd pass, right side
+		std::size_t lowestIdR = N-1, highestIdR = N-1;
+		minY = 101, maxY = -1;
+		for (std::size_t i=trees.size()-1, prev=i; i>=0; i=prev) {
+			while (prev>=0 && trees[prev].x == trees[i].x) prev--;
+			const std::size_t p = prev+1;
+
+			if (trees[i].x == maxX) {
+				lowestIdR = p; highestIdR = i;
+				for (std::size_t j=p; j<=i; j++) removeIds[j] = false;
+			}
+			if (trees[p].y < minY) { minY = trees[p]; lowestIdR  = p; removeIds[p] = false; }
+			if (trees[i].y > maxY) { maxY = trees[i]; highestIdR = i; removeIds[i] = false; }
 		}
 
-		for (auto it=quad0.begin(); *it!=*quad1.begin(); ++it) {
-			for (auto jt=quad0.begin(); jt!=quad0.end(); ) {
-				if (!comps[1](*jt, *it)) jt = quad0.erase(jt);
-				else /*               */ result.push_back(*jt++);
-		} }
-		for (auto it=quad1.begin(); *it!=*quad2.begin(); ++it) {
-			for (auto jt=quad1.begin(); jt!=quad1.end(); ) {
-				if (!comps[2](*jt, *it)) jt = quad0.erase(jt);
-				else /*               */ result.push_back(*jt++);
-		} }
-		for (auto it=quad2.begin(); *it!=*quad3.begin(); ++it) {
-			for (auto jt=quad2.begin(); jt!=quad2.end(); ) {
-				if (!comps[3](*jt, *it)) jt = quad0.erase(jt);
-				else /*               */ result.push_back(*jt++);
-		} }
-		for (auto it=quad3.begin(); *it!=*quad0.begin(); ++it) {
-			for (auto jt=quad3.begin(); jt!=quad3.end(); ) {
-				if (!comps[0](*jt, *it)) jt = quad0.erase(jt);
-				else /*               */ result.push_back(*jt++);
-		} }
+		//Include the possible top and bottom "flat" bits.
+		for (std::size_t i=lowestIdL; i<lowestIdR; i++) {
+			if (trees[i] == )
+			// This is the part I realized I was doing it wrong this whole time. Time for attempt 3.
+		}
 
-		return result;
+		std::size_t count = 0;
+		Iterator end = std::remove_if(
+			trees.begin(), trees.end(),
+			[&](auto) { return removeIds[count++]; }
+		);
+		return trees;
 	}
 };
