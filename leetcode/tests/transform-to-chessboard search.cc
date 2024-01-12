b2447ee77c9d7b76014a9136a51563eb97817fc8
TheZacher5645 <zander5645@yahoo.com>
2024-01-12 05:26:58 -0600

    Finish transform-to-chessboard

--- leetcode/tests/transform-to-chessboard search.cc
+++ leetcode/tests/transform-to-chessboard search.cc
@@ -5,10 +5,9 @@
 
 static constexpr int Max_N = 30;
 class Chessboard {
-	using Bitset = std::bitset<Max_N>;
 	const std::size_t N;
-	Bitset rowDef;
-	Bitset colDef;
+	std::bitset<Max_N> rowDef;
+	std::bitset<Max_N> colDef;
 	bool validity = true;
 
 	static constexpr bool isHalf(std::size_t n, std::size_t x) {
@@ -32,60 +31,25 @@ public:
 		for (std::size_t i=0; i<N && validity; i++) {
 		for (std::size_t j=0; j<N && validity; j++) {
 			bool expected = rowDef[j] ^ colDef[i];
-			if (b[i*N+j] != expected) goto invalid;
+			if (b[i*N+j] != expected) { validity = false; return; }
 		} }
 
 		// Also, make sure the number of 0's
 		// and 1's are as close as possible.
 		for (std::size_t i=0; i<N; i++) {
-			std::size_t rowCount=0, colCount=0;
-			for (std::size_t j=0; j<N; j++) {
-				rowCount += at(i,j);
-				colCount += at(j,i);
-			}
-			if (!isHalf(N, rowCount)) goto invalid;
-			if (!isHalf(N, colCount)) goto invalid;
+			if (!isHalf(N, rowDef.count())) { validity = false; return; }
+			if (!isHalf(N, colDef.count())) { validity = false; return; }
 		}
-
-		// Each solved board has 2 types of rows, and 2
-		// types of columns that occur 'equally' often.
-		for (std::size_t i=0; i<N; i++) {
-			std::unordered_map<Bitset, std::size_t> rowMap, colMap;
-			for (std::size_t j=0; j<N; j++) {
-				rowMap[rowAt(j)]++;
-				colMap[colAt(j)]++;
-			}
-			auto rowCount = rowMap.begin()->second;
-			auto cowCount = colMap.begin()->second;
-			if (rowMap.size() != 2 || !isHalf(N, rowCount)) goto invalid;
-			if (colMap.size() != 2 || !isHalf(N, cowCount)) goto invalid;
-		}
-		return;
-
-	invalid:
-		validity = false;
-		return;
 	};
-
-	bool at(std::size_t i, std::size_t j) const { return rowDef[i] ^ colDef[j]; }
-	Bitset rowAt(std::size_t i) const { return colDef[i] ? ~rowDef : rowDef; }
-	Bitset colAt(std::size_t i) const { return rowDef[i] ? ~colDef : colDef; }
 
 	bool valid() const { return validity; }
 };
 
 int main() {
-	constexpr unsigned N = 3;
+	constexpr unsigned N = 4;
 	for (std::size_t i=0; i<(1<<N*N); i++) {
 		std::bitset<N*N> bits {i};
 		Chessboard b {N, bits};
 		if (b.valid()) std::cout << bits << "\n";
-		// if (b.balanced()
-		// && (!b.balancedRows()
-		// ||  !b.balancedCols())
-		// &&  b.validDuplicateRows()
-		// &&  b.validDuplicateCols()) {
-		// 	std::cout << bits << "\n";
-		// }
 	}
 }
--- leetcode/transform-to-chessboard.cc
+++ leetcode/transform-to-chessboard.cc
@@ -1,31 +1,67 @@
 class Solution {
-	static constexpr int MaxN = 30;
+	static constexpr int Max_N = 30;
+	using Bitset = std::bitset<Max_N>;
+
+	static constexpr Bitset makeGoal(std::size_t N) {
+		Bitset result;
+		for (std::size_t i=0; i<N; i++) result[i] = i&1;
+		return result;
+	}
+
 	class Chessboard {
 		const std::size_t N;
-		std::bitset<MaxN> rowDef;
-		std::bitset<MaxN> colDef;
+		Bitset rowDef;
+		Bitset colDef;
 		bool validity = true;
+
+		static constexpr bool isHalf(std::size_t n, std::size_t x) {
+			return x == n/2 || ((n&1) && x == n/2+1);
+		}
+
 	public:
 		Chessboard(const std::vector<std::vector<int>>& b)
 		: N {b.size()} {
+			// All solvable boards can be defined
+			// by just their 1st row and 1st col.
+			const bool parity = b[0][0];
 			for (std::size_t i=0; i<N; i++) {
-				rowDef[i] = (bool)b[0][i];
-				colDef[i] = (bool)b[i][0];
+				rowDef[i] = b[0][i];
+				colDef[i] = b[i][0] ^ parity;
 			}
-
+			// If this one does not have
+			// that property, reject it.
 			for (std::size_t i=0; i<N && validity; i++) {
 			for (std::size_t j=0; j<N && validity; j++) {
-				bool expected = rowDef[i] ^ colDef[j];
-				if ((bool)b[i][j] != expected) validity = false;
+				bool expected = rowDef[j] ^ colDef[i];
+				if (b[i][j] != expected) { validity = false; return; }
 			} }
-		};
+			// Also, make sure the number of 0's
+			// and 1's are as close as possible.
+			for (std::size_t i=0; i<N; i++) {
+				if (!isHalf(N, rowDef.count())) { validity = false; return; }
+				if (!isHalf(N, colDef.count())) { validity = false; return; }
+			}
+		}
 
-		bool valid() { return validity; }
+		bool   valid()  const { return validity; }
+		Bitset getRow() const { return rowDef; }
+		Bitset getCol() const { return colDef; }
 	};
+
 public:
 	int movesToChessboard(std::vector<std::vector<int>>& b) {
 		const std::size_t N = b.size();
 		Chessboard board {b};
 		if (!board.valid()) return -1;
+
+		std::size_t rowPop = (board.getRow() ^ makeGoal(N)).count();
+		std::size_t colPop = (board.getCol() ^ makeGoal(N)).count();
+		return (N&1)
+			// ODD case
+			? (rowPop&1 ? N-rowPop : rowPop)/2
+			+ (colPop&1 ? N-colPop : colPop)/2
+			// EVEN case
+			: std::min(N-rowPop, rowPop)/2
+			+ std::min(N-colPop, colPop)/2;
 	}
 };
