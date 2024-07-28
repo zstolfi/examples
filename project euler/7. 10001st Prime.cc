bc56c7ef23530b38f80d244526dcda3b358514b0
TheZacher5645 <zander5645@yahoo.com>
2024-07-28 15:23:08 -0500

    Solve Project Euler 7

---
+++ project euler/7. 10001st Prime.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+namespace ranges = std::ranges;
+using UINT = unsigned long long;
+
+int main() {
+	const UINT N = 10'001;
+	auto primes = std::vector<UINT> {};
+	primes.reserve(N);
+
+	for (UINT n=2; ; n++) {
+		auto dividesN = [n](UINT p) { return n%p == 0; };
+		if (ranges::none_of(primes, dividesN)) {
+			primes.push_back(n);
+			if (primes.size() >= N) break;
+		}
+	}
+	std::cout << "N = " << N << "\n";
+	std::cout << "Nth prime: " << primes[N-1] << "\n";
+}
