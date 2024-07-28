8e6ff702e42881e0fc0ed224efc467ca59f3b0a8
TheZacher5645 <zander5645@yahoo.com>
2024-07-28 14:49:55 -0500

    Add Lagrange interpolation
    :)

--- project euler/6. Sum Square Difference.cc
+++ project euler/6. Sum Square Difference.cc
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <iomanip>
+#define LAGRANGE_INTERPOLATION
 #include "polynomial.hh"
 
 int main() {
-	Polynomial sumOfSquares {(double []) {0, 1/6.0, 1/2.0, 1/3.0}};
-	Polynomial squareOfSum  {(double []) {0, 0, 1/4.0, 1/2.0, 1/4.0}};
+	Polynomial sumOfSquares {FromLagrange, (double [][2]) {
+		{0, 0}
+		{1, 1},
+		{2, 1+4},
+		{3, 1+4+9},
+	}};
+
+	Polynomial sum {FromLagrange, (double [][2]) {
+		{0, 0}
+		{1, 1},
+		{2, 1+2},
+	}};
 
 	std::cout << "Difference: " << std::fixed << std::setprecision(0)
-	/*     */ << squareOfSum(100) - sumOfSquares(100) << "\n";
+	/*     */ << sum(100)*sum(100) - sumOfSquares(100) << "\n";
 }
--- project euler/polynomial.hh
+++ project euler/polynomial.hh
@@ -5,6 +5,12 @@
 #include <concepts>
 namespace ranges = std::ranges;
 
+#ifdef LAGRANGE_INTERPOLATION
+	#include <Eigen/Core>
+	#include <Eigen/LU>
+	static struct FromLagrange_tag {} FromLagrange;
+#endif
+
 template <class Number>
 class Polynomial {
 	std::vector<Number> coefficients {Number {0}};
@@ -14,6 +20,10 @@ public:
 	: coefficients{ranges::begin(v), ranges::end(v)} {
 		canonicalize();
 	}
+
+	#ifdef LAGRANGE_INTERPOLATION
+		Polynomial(FromLagrange_tag, ranges::input_range auto&&);
+	#endif
 
 	Number operator()(Number x) const {
 		Number result {0}, power {1};
@@ -40,3 +50,35 @@ private:
 
 template <ranges::input_range IR>
 Polynomial(IR&&) -> Polynomial<ranges::range_value_t<IR>>;
+
+#ifdef LAGRANGE_INTERPOLATION
+	template <class Number>
+	Polynomial<Number>::Polynomial(FromLagrange_tag,
+		ranges::input_range auto&& points
+	) {
+		const std::size_t N = ranges::size(points);
+		auto a = [&](auto i) { return points[i][0]; };
+		auto b = [&](auto i) { return points[i][1]; };
+
+		// https://math.stackexchange.com/a/3253643
+		auto coef = [&](std::size_t ci) -> Number {
+			Eigen::MatrixX<Number> num {N,N}, den {N,N};
+			for (std::size_t i=0; i<N; i++) {
+			for (std::size_t j=0; j<N; j++) {
+				num(i,j) = (i==0)? Number {1}: a(j) * num(i-1,j);
+				den(i,j) = (i==0)? Number {1}: a(j) * den(i-1,j);
+				if      (i == N-1) num(i,j) = b(j);
+				else if (i == ci)  num(i,j) *= a(j);
+			} }
+			return (N-ci & 1? 1: -1) * num.determinant() / den.determinant();
+		};
+
+		coefficients.clear();
+		for (std::size_t i=0; i<N; i++) coefficients.push_back(coef(i));
+		canonicalize();
+	}
+
+	template <ranges::input_range IR>
+	Polynomial(FromLagrange_tag, IR&&)
+	-> Polynomial<ranges::range_value_t<ranges::range_value_t<IR>>>;
+#endif
