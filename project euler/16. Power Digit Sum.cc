ca62958d6aa6a70012dc2cc08780b28943e2236d
TheZacher5645 <zander5645@yahoo.com>
2024-08-11 18:49:37 -0500

    Project Euler 16

---
+++ project euler/16. Power Digit Sum.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "dense_int.hh"
+using UINT = unsigned;
+
+int main() {
+	auto n = Dense{1} << 1000;
+	std::cout << "2^1000 = " << n << "\n";
+
+	UINT sum = 0;
+	for (;n; n/=10) sum += UINT {n%10};
+	std::cout << "Sum: " << sum << "\n";
+}
