ce2175fb3623085291d51bd29f8a45ca851b5755
TheZacher5645 <zander5645@yahoo.com>
2024-08-12 02:57:59 -0500

    Project Euler 20

---
+++ project euler/20. Factorial Digit Sum.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "dense_int.hh"
+using UINT = unsigned;
+
+int main() {
+	Dense product {1};
+	for (UINT i=1; i<=100; i++) product *= i;
+	std::cout << "100! = " << product << "\n";
+
+	UINT sum = 0;
+	for (;product; product/=10) sum += UINT {product%10};
+	std::cout << "Sum: " << sum << "\n";
+}
