703f4ca8c09691f1e03b5d9bf6995bfe9af8acec
zstolfi <zander5645@gmail.com>
2026-01-29 20:46:26 -0600

    Start writing rotate function
    There's clearly something wrong with it at the moment, but it's mostly correct!

--- gidpixhi/main.cc
+++ gidpixhi/main.cc
@@ -4,41 +4,65 @@
 #include <numeric>
 #include <iterator>
 
+struct Ray { Point origin, direction; };
+// https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+std::vector<Point> rotate(
+	std::span<Point const> points,
+	Ray axis,
+	double cos
+) {
+	double sin = std::sqrt(1.0 - cos*cos);
+	double soc = 1.0 - cos;
+	Point const& u = axis.direction;
+	double const matrix[3][3] = {
+		{u.x*u.x*soc + 1.0*cos, u.x*u.y*soc - u.z*sin, u.x*u.z*soc + u.y*sin},
+		{u.x*u.y*soc + u.z*sin, u.y*u.y*soc + 1.0*cos, u.y*u.z*soc - u.x*sin},
+		{u.x*u.z*soc - u.y*sin, u.y*u.z*soc + u.x*sin, u.z*u.z*soc + 1.0*cos},
+	};
+	std::vector<Point> result {points.begin(), points.end()};
+	for (Point& p : result) p = sub(p, axis.origin);
+	for (Point& p : result) {
+		p = Point {
+			.x = matrix[0][0]*p.x + matrix[0][1]*p.y + matrix[0][2]*p.z,
+			.y = matrix[1][0]*p.x + matrix[1][1]*p.y + matrix[1][2]*p.z,
+			.z = matrix[2][0]*p.x + matrix[2][1]*p.y + matrix[2][2]*p.z,
+		};
+	}
+	for (Point& p : result) p = add(p, axis.origin);
+	return result;
+}
+
 struct Net {
 	using SegmentIndex = std::size_t;
 	static constexpr SegmentIndex NoParent = -1;
 	struct Segment {
 		Shape polygon {}; // Face in "assembled" position.
+		Set<Shape::Edge> edges {};
 		Point normal {};
 		SegmentIndex parent = NoParent;
-		double dihedralAngle {};
-		struct { Point rayOrigin, rayDirection; } axis {};
+		Ray axis {};
+		double cosAngle {};
 	};
 	std::vector<Segment> segments {};
 
 	Net(Shape const& shape) {
-		struct Crossing { SegmentIndex si; Shape::Edge edge; };
-		std::map<SegmentIndex, std::vector<Crossing>> graph {};
-		std::map<Shape::Edge, Set<SegmentIndex>> edgeConnections {};
 		// Explode shape into polygonal faces.
 		for (Shape::Face f : shape.faces) {
-			SegmentIndex si = segments.size();
 			Set<Shape::Vertex> faceVertices {};
-			for (auto ei: f) for (auto vi: shape.edges[ei]) {
-				faceVertices.insert(shape.vertices[vi]);
-				edgeConnections[shape.edges[ei]].insert(si);
+			Set<Shape::Edge>   faceEdges    {};
+			for (auto ei: f) {
+				faceEdges.insert(shape.edges[ei]);
+				for (auto vi: shape.edges[ei]) {
+					faceVertices.insert(shape.vertices[vi]);
+				}
 			}
 			segments.emplace_back(
 				Shape {FromVertices, faceVertices},
+				faceEdges,
 				normal(faceVertices)
 				// The rest of the fields to be filled out once the parent
-				// is determined
+				// is determined.
 			);
-		}
-		// Construct graph.
-		for (auto [edge, segmentIndices] : edgeConnections) {
-			graph[segmentIndices[0]].emplace_back(segmentIndices[1], edge);
-			graph[segmentIndices[1]].emplace_back(segmentIndices[0], edge);
 		}
 		// Make all normals point outwards.
 		{
@@ -64,33 +88,58 @@ struct Net {
 				return a.normal.z > b.normal.z;
 			}
 		);
+		// Construct graph.
+		struct Crossing { SegmentIndex si; Shape::Edge edge; };
+		std::map<SegmentIndex, std::vector<Crossing>> graph {};
+		for (SegmentIndex i=0; i<segments.size(); i++) {
+			for (SegmentIndex j=i+1; j<segments.size(); j++) {
+				for (auto edge: segments[i].edges) {
+					if (segments[j].edges.contains(edge)) {
+						graph[i].emplace_back(j, edge);
+						graph[j].emplace_back(i, edge);
+					}
+				}
+			}
+		}
 		// For each non-root node, connect itself to its most upward neighbor.
 		for (SegmentIndex si=1; si<segments.size(); si++) {
 			auto parent = *std::max_element(
 				graph[si].begin(), graph[si].end(),
-				[&](Crossing a, Crossing b) {
-					return segments[a.si].normal.z
-					>      segments[b.si].normal.z;
-				}
+				[&](Crossing a, Crossing b) { return a.si > b.si; }
 			);
 			segments[si].parent = parent.si;
-			// Calculate dihedral angle.
-			segments[si].dihedralAngle = std::acos(
-				dot(segments[si].normal, segments[parent.si].normal)
-			);
 			// Calculate axis of rotation.
-			segments[si].axis.rayOrigin = shape.vertices[parent.edge[0]];
-			segments[si].axis.rayDirection = norm(sub(
-				shape.vertices[parent.edge[0]],
-				shape.vertices[parent.edge[1]]
-			));
+			segments[si].axis.origin = shape.vertices[parent.edge[0]];
+			segments[si].axis.direction = cross(
+				segments[si].normal, segments[parent.si].normal
+			);
+			// Calculate dihedral angle.
+			segments[si].cosAngle = dot(
+				segments[si].normal, segments[parent.si].normal
+			);
 		}
 	}
 
 	std::vector<Shape> interpolate(double t) {
 		std::vector<Shape> result {};
-		for (Segment const& s : segments) {
-			result.push_back(s.polygon);
+		for (SegmentIndex si=0; si<segments.size(); si++) {
+			Set<Point> points {};
+			for (auto ei: segments[si].polygon.faces[0])
+			for (auto vi: segments[si].polygon.edges[ei]) {
+				points.insert(segments[si].polygon.vertices[vi]);
+			}
+			// Rotate segment
+			for (auto node=si; node!=NoParent; node=segments[node].parent) {
+				auto newPoints = rotate(
+					points,
+					segments[node].axis,
+					// Interpolate the angle to 0, and not just the cosAngle.
+					// https://www.desmos.com/calculator/6byuakskl9
+					std::cos(t * std::acos(segments[node].cosAngle))
+				);
+				points = {newPoints.begin(), newPoints.end()};
+			}
+			result.emplace_back(FromVertices, points);
 		}
 		return result;
 	}
@@ -157,5 +206,5 @@ int main() {
 
 //	writePly(std::cout, asPly(cuboctahedron));
 	Net net {cuboctahedron};
-	writePly(std::cout, asPly(net.interpolate(0)));
+	writePly(std::cout, asPly(net.interpolate(1)));
 }
